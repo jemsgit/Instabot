e4c975363a7fcb6aa26442c3c3d4742e69dace6b
Evgeniy Martynov <evgeniy.martynov@artezio.ru>
2019-05-31 19:09:02 +0400

    save objects

--- src/instagram/hashtagObserver.ts
+++ src/instagram/hashtagObserver.ts
@@ -4,13 +4,28 @@ let time = 15000;
 
 async function hashtagObserver(page: puppeteer.Page, hastag: string) {
     let cancelled = false;
+    let photos = {};
 
     async function observe() {
         console.log('start observe')
         await page.goto(`https://www.instagram.com/explore/tags/${hastag}`, {waitUntil: 'networkidle2'});
         let images = await page.$$('article > div:nth-child(3) img');
         images.forEach(async element => {
-            const imgSrc = await page.evaluate(img => img.getAttribute('src'), element);
+            const imgSrc = await page.evaluate(img => 
+                {
+                    let src = img.getAttribute('src')
+                    let parent = img.parentElement;
+                    while(parent != null && parent.nodeName !== "A") {
+                        parent = parent.parent;
+                    }
+                    if(parent) {
+                        let href = parent.getAttribute('href');
+                        if(!photos[href]) {
+                            photos[href] = src;
+                        }
+                    }
+                    return src;
+                }, element);
             console.log(imgSrc);
             await element.dispose();
         });
